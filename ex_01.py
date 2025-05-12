ffd1538253f9e78fa01aa2ae12ddf00ce9ad8b25
Vinicios-Pires <contato.viniciospires@gmail.com>
2025-05-11 22:37:42 -0300

    feat: exercícios referentes a aula 1 do Bootcamp Python Intensivo - Jornada de Dados

---
+++ ex_01.py
@@ -0,0 +1,6 @@
+# Exercício 1
+# Um programa que peça o nome do usuário e imprima o número de letras do nome.
+
+nome = input("Qual é o seu nome? ")
+
+print(len(nome))
---
+++ ex_02.py
@@ -0,0 +1,7 @@
+# Exercício 2
+# Um programa que peça dois números inteiros e imprima a soma deles.
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+
+print("A soma é: " + str(float(num1) + float(num2)))
